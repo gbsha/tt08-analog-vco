be3a8cff1f13150f5c386be1e836664d694a6432
Georg Boecherer <mail@georg-boecherer.de>
2024-09-05 22:34:43 +0200

    vco_core: LVS and DRC clean

--- mag/run-drc.sh
+++ mag/run-drc.sh
@@ -4,4 +4,5 @@ iic-drc.sh -b -w ./drc vco_inverter
 iic-drc.sh -b -w ./drc vco_buffer
 iic-drc.sh -b -w ./drc vco_stage_half
 iic-drc.sh -b -w ./drc vco_stage
+iic-drc.sh -b -w ./drc vco_core
 
--- mag/run-lvs.sh
+++ mag/run-lvs.sh
@@ -4,4 +4,5 @@ iic-lvs.sh -s ../xschem/sim/vco_inverter.spice -l vco_inverter.mag -c vco_invert
 iic-lvs.sh -s ../xschem/sim/vco_buffer.spice -l vco_buffer.mag -c vco_buffer -w ./lvs
 iic-lvs.sh -s ../xschem/sim/vco_stage_half.spice -l vco_stage_half.mag -c vco_stage_half -w ./lvs
 iic-lvs.sh -s ../xschem/sim/vco_stage.spice -l vco_stage.mag -c vco_stage -w ./lvs
+iic-lvs.sh -s ../xschem/sim/vco_core.spice -l vco_core.mag -c vco_core -w ./lvs
 
